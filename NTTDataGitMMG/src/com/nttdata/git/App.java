6b9014669fd0790cc678cba046659e29e9604289
Manoli Mena González <gonzalez.manoli94@gmail.com>
2023-01-05 13:18:38 +0100

    Resubido

---
+++ NTTDataGitMMG/src/com/nttdata/git/App.java
@@ -0,0 +1,29 @@
+package com.nttdata.git;
+
+/**
+ * Clase principal
+ * 
+ * @author manoli
+ *
+ */
+
+public class App {
+	/**
+	 * Método principal
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+
+		// Impresión saludo mundo
+		System.out.println("Hola Mundo");
+
+		// Impresión saludo programador
+		System.out.println("Hola Manoli");
+
+		// Impresión saludo Sevilla
+		System.out.println("Hola Sevilla");
+	}
+
+}
